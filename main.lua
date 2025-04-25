481b0d1caaf5b9a91e5e18717301b5628c1ad66a
SchizoProg <73rayaneh@gmail.com>
2025-04-25 21:35:54 +0200

    fixed links

--- main.lua
+++ main.lua
@@ -6,9 +6,9 @@ if _G.__CHESS_AI_LOADED__FULL then
 end
 _G.__CHESS_AI_LOADED__FULL = true
 
-modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/config.lua"))()
-modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/core/state.lua"))()
-modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/core/ai.lua"))()
-modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/core/gui.lua"))()
+modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/config.lua"))()
+modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/core/state.lua"))()
+modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/core/ai.lua"))()
+modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/core/gui.lua"))()
 
 modules.gui.init(modules)
