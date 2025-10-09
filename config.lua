7397567d84713bee657ef7c36aa269ffb4c2d55e
Saulildo <104403007+Saulildo@users.noreply.github.com>
2025-10-09 16:42:17 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,34 +1,26 @@
-local config = {}
-
--- Clock to wait-time ranges
-config.CLOCK_WAIT_MAPPING = {
-    ["∞"] = {min = 4, max = 7},
-    ["1:00"] = {min = 0, max = 1},
-    ["3:00"] = {min = 2, max = 3},
-    ["10:00"] = {min = 4, max = 7},
-}
-
--- Clock to game type
-config.CLOCK_NAME_MAPPING = {
-    ["1:00"] = "bullet",
-    ["3:00"] = "blitz",
-    ["10:00"] = "rapid",
-    ["∞"] = "casual",
-}
-
--- Icon and colors
-config.ICON_IMAGE = "http://www.roblox.com/asset/?id=95384848753847"
-
-config.COLORS = {
-    on = {
-        background = Color3.fromRGB(255, 170, 0),
-        text = Color3.fromRGB(22, 16, 12),
-        icon = Color3.fromRGB(22, 16, 12),
+local config = {
+    STOCKFISH_URL = "http://localhost:8080",
+    
+    CLOCK_WAIT_MAPPING = {
+        ["∞"] = {min = 4, max = 7},
+        ["1:00"] = {min = 0, max = 1},
+        ["3:00"] = {min = 2, max = 3},
+        ["10:00"] = {min = 4, max = 7},
     },
-    off = {
-        background = Color3.fromRGB(22, 16, 12),
-        text = Color3.fromRGB(255, 170, 0),
-        icon = Color3.fromRGB(255, 170, 0)
+    
+    CLOCK_NAME_MAPPING = {
+        ["1:00"] = "bullet",
+        ["3:00"] = "blitz",
+        ["10:00"] = "rapid",
+        ["∞"] = "casual",
+    },
+    
+    ENGINE = {
+        THREADS = 5,
+        CONTEMPT = 100,
+        HASH = 128,
+        PONDER_DEPTH = 12,
+        MOVES_TO_GO = 40
     }
 }
 
