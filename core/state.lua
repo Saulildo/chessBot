12eb209befe9e0b424a5a0c6fbaf970c811cb985
Saulildo <104403007+Saulildo@users.noreply.github.com>
2025-10-12 18:34:36 -0300

    Update state.lua

--- core/state.lua
+++ core/state.lua
@@ -1,16 +1,9 @@
 local state = {
     aiLoaded = false,
     aiRunning = false,
-    autoMove = true,
     gameConnected = false,
-    currentAnalysisId = nil,
-    isPondering = false,
-    ponderMove = nil,
-    lastPosition = nil,
-    lastPonderFen = nil,
-    gameEnded = false,
-    board = nil,
-    bestMove = nil
+    thread = nil,
+    currentAnalysisId = nil
 }
 
 return state
